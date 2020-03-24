f1127563aae06078da5aa6f95750224bb972a351
Clannis <Dodds1990@gmail.com>
2020-03-24 00:25:19 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,35 @@
+class Author
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def posts
+        Post.all.select {|post| post.author == self}
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        add_post(post)
+    end
+
+    def self.post_count
+        result = 0
+        self.all.each do |author|
+            result += author.posts.count
+        end
+        result
+    end
+end
