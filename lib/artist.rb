e62a957f8f74f351aef3b44e4cab0c9e8c8c4258
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-27 21:55:27 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+
+  attr_accessor :name 
+  attr_reader :songs
+
+  # @@song_count = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    @songs << songs
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+      Song.all.length
+  end
+
+end
