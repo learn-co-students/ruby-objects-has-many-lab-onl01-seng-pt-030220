36d6c352b8b12a5e890177c4da5dba04804e6c3c
alelee93 <leevargasale@gmail.com>
2020-03-27 22:44:32 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Artist
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song)
+        song = Song.new(song)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+        #binding.pry
+    end
+
+
+end
